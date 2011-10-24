92103026e95cded49f0a582f41274e310d2870a0
Crispin Wellington" <retrogradeorbit@gmail.com>
2011-10-24 17:30:35 +0800

    error passthrough code tightening

---
+++ yabife/yabife/fabfile.py
@@ -0,0 +1,87 @@
+### BEGIN COPYRIGHT ###
+#
+# (C) Copyright 2011, Centre for Comparative Genomics, Murdoch University.
+# All rights reserved.
+#
+# This product includes software developed at the Centre for Comparative Genomics 
+# (http://ccg.murdoch.edu.au/).
+# 
+# TO THE EXTENT PERMITTED BY APPLICABLE LAWS, YABI IS PROVIDED TO YOU "AS IS," 
+# WITHOUT WARRANTY. THERE IS NO WARRANTY FOR YABI, EITHER EXPRESSED OR IMPLIED, 
+# INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND 
+# FITNESS FOR A PARTICULAR PURPOSE AND NON-INFRINGEMENT OF THIRD PARTY RIGHTS. 
+# THE ENTIRE RISK AS TO THE QUALITY AND PERFORMANCE OF YABI IS WITH YOU.  SHOULD 
+# YABI PROVE DEFECTIVE, YOU ASSUME THE COST OF ALL NECESSARY SERVICING, REPAIR
+# OR CORRECTION.
+# 
+# TO THE EXTENT PERMITTED BY APPLICABLE LAWS, OR AS OTHERWISE AGREED TO IN 
+# WRITING NO COPYRIGHT HOLDER IN YABI, OR ANY OTHER PARTY WHO MAY MODIFY AND/OR 
+# REDISTRIBUTE YABI AS PERMITTED IN WRITING, BE LIABLE TO YOU FOR DAMAGES, INCLUDING 
+# ANY GENERAL, SPECIAL, INCIDENTAL OR CONSEQUENTIAL DAMAGES ARISING OUT OF THE 
+# USE OR INABILITY TO USE YABI (INCLUDING BUT NOT LIMITED TO LOSS OF DATA OR 
+# DATA BEING RENDERED INACCURATE OR LOSSES SUSTAINED BY YOU OR THIRD PARTIES 
+# OR A FAILURE OF YABI TO OPERATE WITH ANY OTHER PROGRAMS), EVEN IF SUCH HOLDER 
+# OR OTHER PARTY HAS BEEN ADVISED OF THE POSSIBILITY OF SUCH DAMAGES.
+# 
+### END COPYRIGHT ###
+from fabric.api import env
+from ccgfab.base import *
+
+env.app_root = '/usr/local/python/ccgapps/'
+env.app_name = 'yabife'
+env.repo_path = 'yabife'
+env.app_install_names = ['yabife'] # use app_name or list of names for each install
+env.vc = 'mercurial'
+
+env.writeable_dirs.extend([]) # add directories you wish to have created and made writeable
+env.content_excludes.extend([]) # add quoted patterns here for extra rsync excludes
+env.content_includes.extend([]) # add quoted patterns here for extra rsync includes
+env.auto_confirm_purge = False #controls whether the confirmation prompt for purge is used
+
+def deploy(auto_confirm_purge = False):
+    """
+    Make a user deployment
+    """
+    env.auto_confirm_purge = auto_confirm_purge
+    _ccg_deploy_user()
+
+def snapshot(auto_confirm_purge=False):
+    """
+    Make a snapshot deployment
+    """
+    env.auto_confirm_purge=auto_confirm_purge
+    _ccg_deploy_snapshot()
+
+def release(*args):
+    """
+    Make a release deployment
+    """
+    env.auto_confirm=False
+    if len(args):
+        _ccg_deploy_release(tag=args[0])
+    else:
+        _ccg_deploy_release()
+        
+def testrelease(*args):
+    """
+    Make a release deployment using the dev settings file
+    """
+    env.auto_confirm=False
+    if len(args):
+        _ccg_deploy_release(devrelease=True, tag=args[0])
+    else:
+        _ccg_deploy_release(devrelease=True)
+
+def purge(auto_confirm_purge=False):
+    """
+    Remove the user deployment
+    """
+    env.auto_confirm_purge = auto_confirm_purge
+    _ccg_purge_user()
+
+def purge_snapshot(auto_confirm_purge = False):
+    """
+    Remove the snapshot deployment
+    """
+    env.auto_confirm_purge = auto_confirm_purge
+    _ccg_purge_snapshot()
