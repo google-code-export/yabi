a2491f2fc8d177aea7f607f9e82cea9d3dd63abc
Tamas Szabo <tszabo@ccg.murdoch.edu.au>
2013-03-03 09:37:26 +0200

    Adds yabiadmin/static to .hgignore.

--- yabibe/yabibe/connectors/ex/LocalConnector.py
+++ yabibe/yabibe/connectors/ex/LocalConnector.py
@@ -1,3 +1,5 @@
+import io
+import os
 from yabibe.exceptions import ExecutionError
 from ExecConnector import ExecConnector
 
@@ -59,7 +61,13 @@ class LocalExecutionProcessProtocol(protocol.ProcessProtocol):
         
     def errReceived(self, data):
         self.stderr.write(data.replace("\r\n","\n") if self.unify_line_endings else data )
-    
+   
+    def outConnectionLost(self):
+        self.stdout.close()
+   
+    def errConnectionLost(self):
+        self.stderr.close()
+ 
     def processEnded(self, status_object):
         if self.cleanup:
             self.cleanup()
@@ -132,6 +140,22 @@ class StreamLogger(object):
     def write(self,string):
         self.callback(string)
 
+    def close(self):
+        pass
+
+class CompositeStream(object):
+    def __init__(self, *streams):
+        self.streams = streams
+
+    def write(self, data):
+        for stream in self.streams:
+            stream.write(data)
+
+    def close(self):
+        for stream in self.streams:
+            stream.close()
+
+
 class LocalConnector(ExecConnector):
     delay = 0.1
     
@@ -167,8 +191,12 @@ class LocalConnector(ExecConnector):
             sub = Submission(submission)
             sub.render(submission_data)
             
-            outstream = StreamLogger(lambda x: log("submission script stdout:"+x))
-            errstream = StreamLogger(lambda x: log("submission script stderr:"+x))
+            outstream = CompositeStream(
+                            io.FileIO(os.path.join(working, submission_data["stdout"]), "w"),
+                            StreamLogger(lambda x: log("submission script stdout:"+x)))
+            errstream = CompositeStream(
+                            io.FileIO(os.path.join(working, submission_data["stderr"]), "w"),
+                            StreamLogger(lambda x: log("submission script stderr:"+x)))
             
             if len(sub.render().strip()):
                 log("rendered submission script is:\n"+sub.render())
