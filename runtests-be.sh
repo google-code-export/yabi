3715591702c4a22d613e55fdc53b6bb430340bfb
Crispin Wellington" <retrogradeorbit@gmail.com>
2013-03-06 12:02:43 +0800

    merged

--- runtests-be.sh
+++ runtests-be.sh
@@ -3,5 +3,4 @@
 source virt_yabibe/bin/activate
 
 nosetests -v yabibe/tests/
-#nosetests --with-coverage --cover-erase --cover-package=yabibe --cover-html --cover-branche
-s ./tests/*.py
+#nosetests --with-coverage --cover-erase --cover-package=yabibe --cover-html --cover-branches ./tests/*.py
